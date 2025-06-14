f58a6e130e0d720718eaba888775da8d3553cf5e
NicoMele <nicomele0@gmail.com>
2025-06-14 16:57:14 -0300

    se sube aplicacion

---
+++ QR/QR/Program.cs
@@ -0,0 +1,15 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+Document doc = new Document(PageSize.A4);
+string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+string fullPath = Path.Combine(desktopPath, "CodigoQr.pdf");
+
+PdfWriter.GetInstance(doc, new FileStream(fullPath, FileMode.Create));
+doc.Open();
+doc.Add(new Paragraph("Escanee el código Qr"));
+BarcodeQRCode qRCode = new BarcodeQRCode("https://www.youtube.com/watch?v=q1ULJ92aldE&list=PLHCvaq-UcL_Up7l72-rnwbGt_j63geFqd&ab_channel=NoCopyrightSounds", 1000,1000,null);
+Image qrCodeImage = qRCode.GetImage();
+qrCodeImage.ScaleAbsolute(100, 100);
+doc.Add(qrCodeImage);
+doc.Close();
